fcf54c61909f2f5008f2e50e565936ce171587b9
kk0055 <rtsw.lgy@gmail.com>
2021-08-06 16:45:47 +0800

    BaseLayout

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons >
+          <ion-back-button
+           
+          ></ion-back-button>
+        </ion-buttons>
+        <ion-title></ion-title>
+        <ion-buttons >
+        
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <slot />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+export default {
+  props: ["pageTitle", "pageDefaultBackLink"],
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonBackButton,
+    IonButtons,
+  },
+};
+</script>
