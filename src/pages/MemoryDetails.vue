a9b6619b7023c1ade6b0009d06527ce7f9891ece
kk0055 <rtsw.lgy@gmail.com>
2021-08-07 13:28:10 +0800

    MemoryDetails page ダイナミックに修正

--- src/pages/MemoryDetails.vue
+++ src/pages/MemoryDetails.vue
@@ -4,6 +4,12 @@
     :page-title="loadedMemory ? loadedMemory.title : 'Loading...'"
     page-default-back-link="/memories"
   >
+      <h2 v-if="!loadedMemory">Could not find a memory for the given id.</h2>
+      <memory-overview 
+      v-else 
+      :title="loadedMemory.title" 
+      :image="loadedMemory.image" 
+      :description="loadedMemory.description"></memory-overview>
     </base-layout>
   </div>
 </template>
@@ -12,18 +18,18 @@
 export default {
   data() {
     return {
-      memodyId:null
+      memoryId:this.$route.params.id
     }
   },
   computed: {
       loadedMemory() {
-        return this.$store.getters.memory();
+        return this.$store.getters.memory(this.memoryId);
       }
   },
-  watch: {
-    '$route'(currentRoute) {
-      this.memodyId = currentRoute.params.id
-    }
-  }
+  // watch: {
+  //   '$route'(currentRoute) {
+  //     this.memoryId = currentRoute.params.id
+  //   }
+  // }
 }
 </script>
