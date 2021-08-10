9391fc495a2bbd3236c684137c4739c2512eb316
kk0055 <rtsw.lgy@gmail.com>
2021-08-10 23:12:07 +0800

    slot  practice

--- src/practice/Parent.vue
+++ src/practice/Parent.vue
@@ -7,16 +7,28 @@
     <Product name="ガトーショコラ" />
     <!-- Defaultの値 -->
     <Product/>
+    
+    <Slot>スロット</Slot>
+    <Slot>
+   <template v-slot:nm>名前親</template>
+    </Slot>
+    <Slot>
+    <template v-slot:add>渋谷</template>
+    </Slot>
+  
+ 
   </div>
 </template>
 
 <script>
 import Product from "./Product.vue";
+import Slot from "./Slot.vue";
 
 export default {
   name: "App",
   components: {
     Product,
+    Slot,
   },
     data() {
     return {
---
+++ src/practice/Slot.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mycom">
+    <!-- 読み込まれない -->
+   <p>name:<slot>Slot child</slot></p>
+       <!-- 読み込まれる -->
+  <p>name: Slot child</p>
+    <p>nm slot:<slot name="nm">slot name child</slot></p>
+    <p>address slot:<slot name="add">KL</slot></p>
+
+
+  </div>
+</template>
